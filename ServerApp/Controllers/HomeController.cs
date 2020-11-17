6a347f434d40682deeb86967dd3032b9a819c9ae
Tony Lin <tonylinbusiness@gmail.com>
2020-11-16 19:29:18 -0500

    first commit for SportsStore

---
+++ ServerApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServerApp.Models;
+using System.Linq;
+
+namespace ServerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+ public HomeController(DataContext ctx) {
+ context = ctx;
+ }
+
+        public IActionResult Index()
+        {
+ return View(context.Products.First());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
